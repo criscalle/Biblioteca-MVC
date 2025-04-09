b967a5f4c070e088519f70ec5b04fdab6dba62b6
Cris Calle <cris.calle07@gmail.com>
2025-04-09 15:38:07 -0500

    completo

--- Controllers/HistorialController.cs
+++ Controllers/HistorialController.cs
@@ -40,61 +40,6 @@ public class HistorialController : ControllerBase
         return Ok(historial);
     }
 
-
-    /*[HttpPost("CreateHistorial")]
-    public async Task<ActionResult<Historial>> CreateHistorial([FromBody] HistorialDTO historial)
-    {
-        if (historial == null)
-        {
-            return BadRequest("Los datos del prestamo son requeridos.");
-        }
-
-        var existePersona = await _context.Tb_Persona.AnyAsync(p => p.Id == historial.IdPersona);
-        if (!existePersona)
-        {
-            return Conflict("La persona no se encuentra registrada");
-        }
-
-        var historialEntity = new Historial
-        {
-            Id = historial.Id,
-            IdMaterial = historial.IdMaterial,
-            IdPersona = historial.IdPersona,
-            CreatedDate = DateTime.UtcNow
-        };
-
-        _context.Tb_Historial.Add(historialEntity);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(GetHistorial), new { Id = historialEntity.Id }, historialEntity);
-       
-    }
-
-    [HttpPut("UpdateHistorial/{id}")]
-    public async Task<ActionResult<Historial>> UpdateHistorial(int id, [FromBody] HistorialDTO historial)
-    {
-        if (historial == null)
-        {
-            return BadRequest("Los datos del prestamo son requeridos.");
-        }
-
-        var historialEntity = await _context.Tb_Historial.FirstOrDefaultAsync(p => p.Id == id);
-        if (historialEntity == null)
-        {
-            return NotFound("No se encontró un historial con .");
-        }
-
-        historialEntity.Id = historial.Id;
-        historialEntity.IdMaterial = historial.IdMaterial;
-        historialEntity.IdPersona = historial.IdPersona;
-        historialEntity.LastModifiedDate = DateTime.UtcNow;
-
-        _context.Tb_Historial.Update(historialEntity);
-        await _context.SaveChangesAsync();
-
-        return Ok(historialEntity);
-    }
-    */
     [HttpPost("prestar")]
     public async Task<IActionResult> PrestarMaterial(int personaId, int materialAcademicoId)
     {
